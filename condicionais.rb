63144c49b9e0f70ce1be91781e78d9b6d80f422d
Mateus Sousa <contato.matthewd@gmail.com>
2017-08-08 16:57:07 -0300

    Add files via upload

--- condicionais.rb
+++ condicionais.rb
@@ -1,14 +1,41 @@
-$name = ''
-$age = 0
+$arr = Array.[]
+option = 0
 
-puts "qual seu nome?"
-$nome = STDIN.gets
-
-puts "qual seu nome?"
-$age = STDIN.gets.to_i
-
-if $age >= 20
-  puts "#{$name} to old"
-else
-  puts "#{$name} to young"
+def add()
+  puts "What is your name?"
+  name = gets.chomp.to_s
+  $arr.push(name)
 end
+def list()
+  $arr.each do |item|
+    puts item
+  end
+end
+def remove()
+  puts "Remove What name?"
+  name = gets.chomp.to_s
+  index = $arr.index(name)
+  $arr.delete_at(index)
+end
+
+begin
+  puts ' 1 - Add '
+  puts ' 2 - List '
+  puts ' 3 - Remove '
+  puts ' 4 - Exit '
+
+  option = gets.chomp.to_i
+
+  case option
+  when 1
+    add()
+  when 2
+    list()
+  when 3
+    remove()
+  when 4
+    puts "Até breve"
+  else
+    puts 'Opção invalida'
+  end
+end while option != 4
